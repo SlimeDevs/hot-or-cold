92da39904fed692d34b91547052993c6ada6ef63
SlimeDevs <46899559+SlimeDevs@users.noreply.github.com>
2019-07-21 12:02:11 -0500

    Added javascript to complete the challenge

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+var min = 1;
+var max = 100;
+var numberToGuess;
+var currentGuess = 0;
+
+function newGame() {
+	resetGame();
+	$('#js-guess-submit').parent().submit(function(event) {
+		let guess = $('#js-user-guess').val();
+		$('#guessList').prepend(`<li>${guess}</li>`)
+		currentGuess++;
+		$('.js-guess-count').text(`${currentGuess}`)
+		$('.js-feedback').text(`${determineAccuracy(guess)}`)
+		event.preventDefault();	
+	})
+}
+
 function handleInstructionsModal() {
 	// when users click on the element with
 	// `.js-what` class, we'll fade in
@@ -11,17 +28,42 @@ function handleInstructionsModal() {
 	// the instructions modal
 	$('.js-close').click(function(){
   		$(".overlay").fadeOut(1000);
-  	});
+	});
+
+	$('.js-new-game').click(function() {
+		newGame();
+	});
 }
 
+// Returns a string containing feedback on how close the person is to the number
+function determineAccuracy(number) {
+	if (number >= numberToGuess + 50 || number <= numberToGuess - 50) {
+		return 'Ice cold';
+	} else if (number >= numberToGuess + 30 || number <= numberToGuess - 30) {
+		return 'Cold';
+	} else if (number >= numberToGuess + 20 || number <= numberToGuess - 20) {
+		return 'Warm';
+	} else if (number >= numberToGuess + 10 || number <= numberToGuess - 10) {
+		return 'Hot';
+	} else if (number >= numberToGuess + 1 || number <= numberToGuess - 1) {
+		return 'Very Hot';
+	} else {
+		return 'You got it!';
+	}
+}
 
-// `$(document).ready` lets you specify a
-// function that should execute when all the
-// resources required by your web page have loaded.
-// This code says, when the document is ready, run the
-// `handleInstructionsModal` function.
+function resetGame() {
+	$('.js-guess-list').empty();
+	$('.js-guess-count').text('0');
+	$('.js-feedback').text('Make your Guess!');
+	$('#js-user-guess').val('');
+	numberToGuess = Math.floor(Math.random() * (max - min + 1)) + min;
+	currentGuess = 0;
+}
+
 $(document).ready(function(){
 	handleInstructionsModal();
+	newGame();
 });
 
 
